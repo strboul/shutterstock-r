05e8e4bb2841d45ae89f03f7f103283946e8f2c1
strboul <25015317+strboul@users.noreply.github.com>
2018-11-09 01:24:58 +0200

    Authentication updates
    - Update documentation
    - Added option to return cached token if exists

--- R/authentication.R
+++ R/authentication.R
@@ -1,11 +1,26 @@
 
 #' Authorize Shutterstock
 #'
-#' Authenticate user and store user credentials inside the \code{.httr-oauth}
-#' file placed in the working directory.
+#' Authenticate and store user credentials to authorize requests for the
+#' Shutterstock API. After a successful authentication, an OAuth token will be
+#' cached inside the \code{.httr-oauth}, which is a file placed in the current
+#' working directory. This workflow is mainly followed by the \code{httr}
+#' package.
+#'
 #' @importFrom httr oauth_app oauth_callback oauth2.0_token
 #' @export
 sstk_auth <- function() {
+
+  # return token from disk if file exists:
+  file <- ".httr-oauth"
+  if (file.exists(file)) {
+    message(paste(
+      "'", file, "' found in the directory.\n",
+      "Already authenticated.\n",
+      sep = ""))
+    return(sstk_oauth_token())
+  }
+
   oauth_app <- httr::oauth_app(
     appname = "shutterstock",
     key = sstk_id(),
@@ -30,6 +45,9 @@ sstk_oauth_endpoint <- function() {
   )
 }
 
+#' Read OAuth token from .httr-oauth file
+#'
+#' @export
 sstk_oauth_token <- function() {
   file <- ".httr-oauth"
   if (!file.exists(file)) {
@@ -38,8 +56,16 @@ sstk_oauth_token <- function() {
       "Please authenticate first with sstk_auth()",
       sep = ""), call. = FALSE)
   }
-
   token <- readRDS(file)
+  token
+}
+
+#' OAuth token credentials
+#'
+#' To be added in the http header section.
+#' @noRd
+sstk_oauth_token_cred <- function() {
+  token <- sstk_oauth_token()
   cred <- token[[1]][["credentials"]]
   paste(cred[["token_type"]], cred[["access_token"]])
 }
