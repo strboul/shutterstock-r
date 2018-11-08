6aa7c9c8bfd6b499ed38eab707c37c95a5e1dcc4
strboul <25015317+strboul@users.noreply.github.com>
2018-11-09 00:22:19 +0200

    Create oauth_token function

--- R/authentication.R
+++ R/authentication.R
@@ -1,16 +1,16 @@
 
 #' Authorize Shutterstock
 #'
-#' In principle, user credentials are stored inside the \code{.httr-oauth} in
-#' the working directory.
-#'
+#' Authenticate user and store user credentials inside the \code{.httr-oauth}
+#' file placed in the working directory.
+#' @importFrom httr oauth_app oauth_callback oauth2.0_token
 #' @export
 sstk_auth <- function() {
   oauth_app <- httr::oauth_app(
     appname = "shutterstock",
     key = sstk_id(),
     secret = sstk_secret(),
-    redirect_uri = sstk_callback()
+    redirect_uri = httr::oauth_callback()
   )
 
   token <- httr::oauth2.0_token(
@@ -28,4 +28,18 @@ sstk_oauth_endpoint <- function() {
     "authorize" = "https://api.shutterstock.com/v2/oauth/authorize",
     "access" = "https://api.shutterstock.com/v2/oauth/access_token"
   )
+}
+
+sstk_oauth_token <- function() {
+  file <- ".httr-oauth"
+  if (!file.exists(file)) {
+    stop(paste(
+      "'", file, "' not found.\n",
+      "Please authenticate first with sstk_auth()",
+      sep = ""), call. = FALSE)
+  }
+
+  token <- readRDS(file)
+  cred <- token[[1]][["credentials"]]
+  paste(cred[["token_type"]], cred[["access_token"]])
 }
