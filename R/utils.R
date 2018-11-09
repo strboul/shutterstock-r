98c2e8b67483e8d5be2d3002bf2140cc10ca011d
strboul <25015317+strboul@users.noreply.github.com>
2018-11-09 02:12:32 +0200

    Create stop_error_status function
    Similar to what httr::stop_for_status does

--- R/utils.R
+++ R/utils.R
@@ -22,6 +22,36 @@ check_http_type <- function(resp, type = c("application/json")) {
   }
 }
 
+#' HTTP error message
+#'
+#' When an error occurs during the connection, an explanatory error message is
+#' thrown for particular HTTP error status codes. The call is mostly relied on
+#' \code{\link[httr]{http_error}} function.
+#'
+#' @importFrom httr http_error status_code http_condition
+#' @noRd
+stop_error_status <- function(resp) {
+  stopifnot(inherits(resp, "response"))
+  if (httr::http_error(resp)) {
+    # additional descriptions by the HTTP error status codes:
+    descrp <- switch(
+      as.character(httr::status_code(resp)),
+      "401" = "Authenticate with sstk_auth()",
+      "404" = "That resource does not seem to exist",
+      "403" = "May need to authenticate with sstk_auth()"
+    )
+    cond <- httr::http_condition(resp, "error")
+    stop(
+      sprintf(
+        "Shutterstock API request failed:\n %s\n %s",
+        cond[["message"]],
+        if (identical(length(descrp), 1L)) descrp else ""
+      ),
+      call. = FALSE
+    )
+  }
+}
+
 #' Fetch and parse content of a response
 #'
 #' Advised to use \code{"text"} for \code{as} argument in
