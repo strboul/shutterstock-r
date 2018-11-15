83c36fb569b821f5e39758c7514e972b2c878936
Metin Yazici <25015317+strboul@users.noreply.github.com>
2018-11-15 22:11:21 +0100

    Add new connection calls
    - send_request
    _ get_response

--- R/connection.R
+++ R/connection.R
@@ -1,4 +1,37 @@
 
+# HTTP connection calls
+
+#' Send request
+#'
+#' Bring everything together. First, get a response from server. Second, stop if
+#' response returns an error status. Then, check if http type matches. Last,
+#' return parsed content.
+#'
+#' @noRd
+send_request <- function(...) {
+  resp <- get_response(...)
+  stop_error_status(resp)
+  check_http_type(resp)
+  return_content(resp)
+}
+
+#' Get response
+#'
+#' @param resource character. A Shutterstock API resource.
+#' @param params list. query parameters.
+#' @importFrom httr modify_url GET add_headers
+#' @noRd
+get_response <- function(resource, params) {
+  stopifnot(is.character(resource))
+  stopifnot(is.list(params))
+  auth <- sstk_oauth_token_cred()
+  url <- httr::modify_url(
+    paste0(getOption("sstk.api.root.url"), resource),
+    query = params
+  )
+  httr::GET(url, httr::add_headers(Authorization = auth))
+}
+
 #' Check the content type of response
 #'
 #' Checks Content-Type of an HTTP response. If it returns an error because of a
@@ -7,10 +40,10 @@
 #'
 #' @importFrom httr http_type
 #' @noRd
-check_http_type <- function(resp, type = c("application/json")) {
-  stopifnot(inherits(resp, "response"))
+check_http_type <- function(response, type = c("application/json")) {
+  stopifnot(is.response(response))
   selected.type <- match.arg(type)
-  actual <- httr::http_type(resp)
+  actual <- httr::http_type(response)
   if (!identical(actual, selected.type)) {
     stop(paste(
       "'", actual, "'",
@@ -30,17 +63,17 @@ check_http_type <- function(resp, type = c("application/json")) {
 #'
 #' @importFrom httr http_error status_code http_condition
 #' @noRd
-stop_error_status <- function(resp) {
-  stopifnot(inherits(resp, "response"))
-  if (httr::http_error(resp)) {
+stop_error_status <- function(response) {
+  stopifnot(is.response(response))
+  if (httr::http_error(response)) {
     # additional descriptions by the HTTP error status codes:
     descrp <- switch(
-      as.character(httr::status_code(resp)),
+      as.character(httr::status_code(response)),
       "401" = "Authenticate with sstk_auth()",
       "404" = "That resource does not seem to exist",
       "403" = "May need to authenticate with sstk_auth()"
     )
-    cond <- httr::http_condition(resp, "error")
+    cond <- httr::http_condition(response, "error")
     stop(
       sprintf(
         "Shutterstock API request failed:\n %s\n %s",
@@ -59,9 +92,14 @@ stop_error_status <- function(resp) {
 #'
 #' @importFrom httr content
 #' @noRd
-prepare_content <- function(resp) {
-  stopifnot(inherits(resp, "response"))
-  cont <- httr::content(resp, as = "text")
+return_content <- function(response) {
+  stopifnot(is.response(response))
+  cont <- httr::content(response, as = "text")
 
   jsonlite::fromJSON(cont, simplifyVector = FALSE)
+}
+
+#' Check if an object has response class
+is.response <- function(x) {
+  inherits(x, "response")
 }
