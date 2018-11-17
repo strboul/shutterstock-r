dd2d58d1c68b6be40dac4a157b1bcb3f4e3dd441
Metin Yazici <25015317+strboul@users.noreply.github.com>
2018-11-17 22:56:23 +0100

    Update images call endpoints

--- R/images.R
+++ R/images.R
@@ -1,48 +1,156 @@
 
+### ----------------------------------------------------------------- ###
+### SEARCH ----
+### ----------------------------------------------------------------- ###
+
 #' Search images
 #'
-#' Searching for images. Use the parameters specified in the link
-#' \href{here}{https://api-reference.shutterstock.com/#images}.
+#' Searching for images.
 #'
-#' @param ... additional arguments to be passed by \code{images/search} API
-#'   endpoint parameters.
+#' @param ... arguments to be passed by the parameters in the source endpoint.
+#' @source \code{\dQuote{images/search}}
 #' @examples \dontrun{
-#'   today <- as.character(Sys.Date())
-#'   searchImages(query = "farmer", sort = "popular", added_date = today)
+#' today <- as.character(Sys.Date())
+#' searchImages(query = "farmer", sort = "popular", added_date = today)
 #' }
 #' @export
 searchImages <- function(...) {
   params <- list(...)
-  send_request("images/search", params)
+  send_request(method = "GET",
+               resource = "images/search",
+               parameters = params
+  )
 }
 
 #' List image categories
 #'
 #' Listing the categories (Shutterstock-assigned genres) that images can belong
-#' to. This call takes no parameters.
+#' to. \emph{This call takes no additional parameters.}
 #'
+#' @source \code{\dQuote{images/categories}}
 #' @examples \dontrun{
-#'    listImageCategories()
+#' listImageCategories()
 #' }
 #' @export
 listImageCategories <- function() {
-  send_request("images/categories")
+  send_request(method = "GET",
+               resource = "images/categories"
+  )
 }
 
 #' List similar images
 #'
 #' Returning images that are visually similar to an image that you specify.
 #'
-#' @param id character. Image ID.
-#' @param ... additional arguments to be passed by \code{images/similar} API
-#'   endpoint parameters.
+#' @param id character. Image ID. (required)
+#' @source \code{\dQuote{images/\{id\}/similar}}
 #' @examples \dontrun{
-#'    listSimilarImages(id = "465011609")
+#' listSimilarImages(id = "465011609")
 #' }
 #' @export
 listSimilarImages <- function(id, ...) {
-  if (missing(id)) require_arg(id)
-  assert_type(id, "character")
+  check_required_args(id, "character")
   params <- list(...)
-  send_request(sprintf("images/%s/similar", id), params)
+  send_request(method = "GET",
+               resource = sprintf("images/%s/similar", id),
+               parameters = params
+  )
 }
+
+#' List recommended images
+#'
+#' Returning images that customers put in the same collection as the specified
+#' image IDs.
+#'
+#' @param id character. One or more Image IDs.
+#' @source \code{\dQuote{images/recommendations}}
+#' @examples \dontrun{
+#' listRecommendedImages(id = "465011609")
+#' listRecommendedImages(id = c("465011609", "416256163"), max_items = 5)
+#' }
+#' @export
+listRecommendedImages <- function(id, ...) {
+  check_required_args(id, "character")
+  params <- list("id" = id, ...)
+  send_request(method = "GET",
+               resource = "images/recommendations",
+               parameters = params
+  )
+}
+
+### ----------------------------------------------------------------- ###
+### DETAILS ----
+### ----------------------------------------------------------------- ###
+
+#' Get details about images
+#'
+#' Showing information about an image, including a URL to a preview image and
+#' the sizes that it is available in.
+#'
+#' @param id character. Image ID. (required)
+#' @source \code{\dQuote{images/\{id\}}}
+#' @examples \dontrun{
+#' getImageDetails(id = "465011609")
+#' }
+#' @export
+getImageDetails <- function(id, ...) {
+  check_required_args(id, "character")
+  params <- list(...)
+  send_request(method = "GET",
+               resource = sprintf("images/%s", id),
+               parameters = params
+  )
+}
+
+#' List images
+#'
+#' Listing information about one or more images, including the available sizes.
+#'
+#' @param id character. One or more Image IDs.
+#' @source \code{\dQuote{v2/images/}}
+#' @examples \dontrun{
+#' listImages(id = c("1110335168", "465011609"), view = "minimal")
+#' }
+#' @export
+listImages <- function(id, ...) {
+  check_required_args(id, "character")
+  params <- list("id" = id, ...)
+  send_request(method = "GET",
+               resource = "images",
+               parameters = params
+  )
+}
+
+#' License images
+#'
+#' Getting licenses for one or more images.
+#'
+#' @examples \dontrun{
+#' licenseImages(subscription_id = "<SUBSCRIPTION_ID>", format = "jpg", size =
+#' "small", search_id = "Books")
+#' }
+#' @export
+licenseImages <- function(...) {
+  params <- list(...)
+  send_request(method = "POST",
+               resource = "images/licenses",
+               parameters = params
+  )
+}
+
+#' List licenses
+#'
+#' Listing existing licenses.
+#'
+#' @examples \dontrun{
+#' listLicenses(image_id = "620154782")
+#' }
+#' @export
+listLicenses <- function(...) {
+  params <- list(...)
+  send_request(method = "GET",
+               resource = "images/licenses",
+               parameters = params
+  )
+}
+
