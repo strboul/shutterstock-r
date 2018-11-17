1f9b4b9fcabed897f37b178f31567cc9093e3be9
Metin Yazici <25015317+strboul@users.noreply.github.com>
2018-11-17 13:48:58 +0100

    Insert options to pkg documentation

--- R/shutterstock.R
+++ R/shutterstock.R
@@ -1,6 +1,16 @@
 
 #' Shutterstock API client
 #'
+#' The endpoint descriptions are retrieved from Shutterstock API Reference
+#' documentation.
+#'
+#' @section Package options:
+#' \describe{
+#' \item{\code{sstk.api.root.url}}{The base URL for retrieving the API
+#' endpoints. Default: \code{https://api.shutterstock.com/}}
+#' }
+#'
 #' @references Shutterstock API Reference:
 #'   \url{https://api-reference.shutterstock.com/}
+#' @keywords internal
 "_PACKAGE"
