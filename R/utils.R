6183fb18c9d9e8ffd65e8e3778bdbf16ee4da374
Metin Yazici <25015317+strboul@users.noreply.github.com>
2018-11-25 19:29:42 +0100

    Improve required args error

--- R/utils.R
+++ R/utils.R
@@ -19,14 +19,18 @@ get_environment <- function(which = 1, exclude = NA_character_) {
 
 #' Check required args for the API endpoint parameters
 #' @noRd
-check_required_args <- function(x, type = c("character", "numeric", "integer", "data.frame")) {
-  if (missing(x)) require_args_error()
+check_required_args <- function(x, type = c("character", "numeric", "integer", "data.frame", "list")) {
+  if (missing(x)) {
+    x.name <- deparse(substitute(x))
+    require_args_error(x.name)
+  }
   selected.type <- match.arg(type)
   switch (selected.type,
     "character" = is.character,
     "numeric" = is.numeric,
     "integer" = is.integer,
-    "data.frame" = is.data.frame
+    "data.frame" = is.data.frame,
+    "list" = is.list
   ) -> funTypeCheck
   if (!funTypeCheck(x)) {
     stop(paste(x, "not", selected.type), call. = FALSE)
@@ -35,9 +39,9 @@ check_required_args <- function(x, type = c("character", "numeric", "integer", "
 
 #' Require call arguments
 #' @noRd
-require_args_error <- function() {
+require_args_error <- function(x) {
   stop(paste(
-    "Please provide required parameters."
+    "Please provide required parameter(s):", x
   ),
   call. = FALSE)
 }
