21a80809df0e25295ad0856071094c4d726c88e1
Metin Yazici <25015317+strboul@users.noreply.github.com>
2018-12-02 20:53:06 +0100

    Update documentation

--- R/utils.R
+++ R/utils.R
@@ -1,22 +1,4 @@
 
-#' Get parent call environment variables
-#'
-#' Return all function argument names and values - and exclude some, if
-#' required. \code{sys.frames()} accesses to different call environments in the
-#' call stack.
-#'
-#' @param which integer. the frame number in the call stack.
-#' @param exclude character. which arguments should be excluded?
-#' @seealso \code{?sys.parent}
-#' @noRd
-get_environment <- function(which = 1, exclude = NA_character_) {
-  stopifnot(is.numeric(which))
-  stopifnot(is.character(exclude))
-  e <- sys.frames()[[which]]
-  args <- as.list(e, all.names = TRUE)
-  args[names(args)[!names(args) %in% exclude]]
-}
-
 #' Check required args for the API endpoint parameters
 #' @noRd
 check_required_args <- function(x, type = c("character", "numeric", "integer", "data.frame", "list")) {
